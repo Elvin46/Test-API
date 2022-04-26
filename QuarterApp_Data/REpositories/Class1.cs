2694b5bc8a6f7f4684891402cc9ceec765081c3c
Elvin46 <elvinva@code.edu.az>
2022-04-26 23:23:16 +0400

    qaqaqaq

---
+++ QuarterApp_Data/REpositories/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuarterApp_Data.REpositories
+{
+    internal class Class1
+    {
+    }
+}
