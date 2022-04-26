9c918970808b67cbe8c6f7c40efcf7bf5c7b9b8c
IlkinBairamov <ilkinabr@code.edu.az>
2022-04-26 23:27:34 +0400

    s

---
+++ QuarterApp_Data/REpositories/Class2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuarterApp_Data.REpositories
+{
+    class Class2
+    {
+        public int MyProperty { get; set; }
+    }
+}
